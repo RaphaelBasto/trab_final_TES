dc91f12b74937afc5f59c172746cb5fe5515f469
Raphael Basto <raphaelpgpb@icloud.com>
2022-08-01 00:12:46 -0300

    final

--- conexao.php
+++ conexao.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 
 $hostname = "us-cdbr-east-06.cleardb.net";
-$banco = "raphaelbasto";
+$banco = "heroku_4cf7aeee71cae59";
 $username = "bbec8ec88a2b5f"; 
 $password = "3a9c327b"; 
 
