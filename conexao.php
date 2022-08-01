1179c7d66cd1e6e4d848921b5c0157219442eeb4
Raphael Basto <raphaelpgpb@icloud.com>
2022-07-31 23:47:14 -0300

    update

--- conexao.php
+++ conexao.php
@@ -3,10 +3,10 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 
-$hostname = "localhost";
+$hostname = "us-cdbr-east-06.cleardb.net";
 $banco = "raphaelbasto";
-$username = "root"; //"cakephp";
-$password = ""; //"Cake#123";
+$username = "bbec8ec88a2b5f"; 
+$password = "3a9c327b"; 
 
 $mysqli =  new \MySQLi($hostname, $username, $password, $banco);
 
